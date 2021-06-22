ff7f9845e0dd7685d4b52c53933f113cc851d1ca
John_29 <86144186+Evgeniy29@users.noreply.github.com>
2021-06-22 12:35:02 +0200

    Add files via upload

---
+++ ConsoleApplication_HelloWorld.cpp
@@ -0,0 +1,21 @@
+﻿// Create by John_29
+
+
+#include <iostream>
+
+
+
+/*int main()
+{
+    std::cout << "Hello World!\n"; //сокращенный вариант
+    return 0;
+
+}
+*/
+int main()
+{
+    std::cout << "Hello World!" << std::endl; 
+    return 0;
+
+}
+
