14686656775764c205ef7ed7df6e1413c79f77fe
John29 <john290883@mail.ru>
2021-07-12 09:27:38 +0300

    урок 5 ,6

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,90 @@
+﻿//Create by John_29
+
+#include <iostream>
+
+void arr(double a[], double n)  //task_1
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << a[i] <<" ";
+    }
+    std::cout << std::endl;
+    std::cout <<"----------------------task_1"<< std::endl;
+   
+}
+
+void arr2(int ar[], int n)          //task_2
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << ar[i]<<"";
+    }
+    std::cout << std::endl;
+}
+
+void arr0_1(int ar[], int n)
+{
+   
+        for (int i = 0; i < n; i++)
+            {
+             ar[i] =(ar[i] > 0) ? 0 : 1;
+               
+            std::cout << ar[i];
+            }
+        std::cout << std::endl;
+        std::cout << "----------------------task_2" << std::endl;
+}
+
+void arr_3(int ar[], int n)     //task_3
+{
+    for (int i = 0; i < n; i++)
+    {
+        ar[i] = i * 3;
+        std::cout << ar[i]+1 << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "----------------------task_3" << std::endl;
+}
+
+void arr_sdvig(int ar[], int  n,int sdvig)       //task_4
+{     
+
+        for (int i = 0; i < n; i++)
+        
+            std::cout << ar[i]<<" ";
+        std::cout << std::endl;
+        int t = ar[0];
+            for (int j = 0; j < n ; j++)
+            {
+                if (j == n - sdvig)
+                {
+                    ar[j] = t;
+                    break;
+                }
+                ar[j] = ar[j + sdvig];
+                
+            }
+            for (int j = 0; j < n; j++)
+            {
+                std::cout << ar[j] << " ";
+            }
+        
+       
+}
+int main()
+{
+    int s = 1;
+    int y = 8;
+    int myarr3[8];
+    int z = 5;
+    double myarr[] = { 1.1,2.2,3.3,4.4,5.5 };
+    int myarr4[5] = { 1,2,3,4,5 };
+     arr(myarr,z);
+
+     int myarr2[] = { 0,1,0,1,0 };
+     arr2(myarr2, z);
+     arr0_1(myarr2,z);
+     arr_3(myarr3, y);
+     arr_sdvig(myarr4, z,s);
+}
+
---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,164 @@
+﻿// Create by John_29
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+void stepen_2(int *ar,int n)        //task_1
+{
+    std::cout << "Введите размер массива: ";
+    std::cin >> n;
+    ar = new int[n];
+    int t = 1;
+    std::cout << "Вывод массива: ";
+    for (int i = 0; i < n; i++)
+        {
+            ar[i] = t;
+            std::cout << ar[i] << " ";
+            t *= 2;
+        }
+    std::cout << std::endl;
+    delete[] ar;
+}
+
+void matrix_rand(int **ar,int n,int m)      //task_2
+{
+    
+    
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            {
+        
+                 ar[i][j] = rand() % 100;
+        
+            }
+    
+            
+    
+}
+
+void matrix_cout(int** ar, int n, int m)
+{
+    std::cout << "Вывод матрицы : " << std::endl;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+
+                std::cout << ar[i] [j]<<" ";
+                
+            }
+            std::cout << std::endl;
+        }
+        
+}
+
+void fail_1()       //task_3
+{
+    string textfail;
+     string text1 = "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww";
+    cout << "Введите название файла №1: ";
+    cin >> textfail;
+    textfail += ".txt";
+    
+    ofstream fout( textfail);
+   
+
+    fout << text1 << endl;
+
+    fout.close();
+}
+void fail_2()
+{
+    string textfail;
+    string text2 = "qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq";
+    cout << "Введите название файла №2: ";
+    cin >> textfail;
+    textfail += ".txt";
+    ofstream fout(textfail);
+
+    fout << text2 << endl;
+
+    fout.close();
+}
+
+void sliyanieFails()        //task_4
+{
+    string textfail1;
+    string text1 = "* Написать функцию, «склеивающую» эти файлы в третий текстовый файл (имя файлов спросить у пользователя).";
+    string textfail2;
+    string text2 = "* Написать программу, которая проверяет присутствует ли указанное пользователем при запуске программы слово в указанном пользователем файле (для простоты работаем только с латиницей).";
+    string textfail3;
+    cout << "Введите название файла №1: ";
+    cin >> textfail1;
+    textfail1 += ".txt";
+    ofstream fout1(textfail1);
+    if (fout1.is_open())
+    {
+        fout1 << text1;
+    }
+    cout << "Введите название файла №2: ";
+    cin >> textfail2;
+    textfail2 += ".txt";
+    ofstream fout2(textfail2);
+    if (fout2.is_open())
+    {
+        fout2 << text2;
+    }
+    cout << "Введите название файла слияния №3: ";
+    cin >> textfail3;
+    textfail3 += ".txt";
+    ofstream fout3(textfail3);
+    fout3 << text1 << text2;
+    fout1.close();
+    fout2.close();
+    fout3.close();
+   
+}
+
+void poisk()        //task_5
+{
+    string str = "Урок 6. Управление памятью. Ссылки и указатели, именование, арифметика, функции обратного вызова";
+    ofstream fout("textfail.txt");
+    if (fout.is_open())
+    {
+        fout << str;
+    }
+    
+    if (str.find("Ссылки") != string::npos)
+    {
+        cout << "Найдено";
+    }
+    else
+    {
+        cout << "Не найдено";
+    }
+    fout.close();
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    
+    int z=4;
+    int y=4;
+    int s=5;
+    int** a;
+    a = new int* [z];
+    for (int i=0; i < z; i++)
+        a[i] = new int[y];
+    stepen_2(&z,s);
+    matrix_rand(a,z,y);
+    matrix_cout(a,z,y);
+    for (int i=0; i < z; i++)
+        delete[] a[i];
+    delete[] a;
+    fail_1();
+    fail_2();
+    sliyanieFails();
+    poisk();
+    return 0;
+}
